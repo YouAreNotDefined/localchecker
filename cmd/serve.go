33cac5fab4c285ac447945d72fae0b2dd0284d8d
YouAreNotDefined <for.1self.ik@gmail.com>
2021-07-23 07:07:40 +0000

    Fix the order in which functions are executed

--- cmd/serve.go
+++ cmd/serve.go
@@ -37,23 +37,26 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	c := make(chan Res)
 
 	if rImg.MatchString(reqUri) {
-		go getImage(reqUri, c)
+		go getData(reqUri, c)
 
 	} else if rOther.MatchString(reqUri) {
-		if strings.Contains(reqUri, "css") {
+		if strings.Contains(reqUri, ".css") {
 			w.Header().Set("Content-Type", "text/css; charset=utf-8")
-		} else if strings.Contains(reqUri, "js") {
+		} else if strings.Contains(reqUri, ".js") {
 			w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
-		} else if strings.Contains(reqUri, "json") {
+		} else if strings.Contains(reqUri, ".json") {
 			w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		}
-		go rewrite(reqUri, c)
+		go getData(reqUri, c)
+		go rewrite(<-c, c)
 
 	} else {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		go rewrite(reqUri, c)
-		go includeReplace(<-c, c)
+		go getData(reqUri, c)
+		go rewrite(<-c, c)
 		go includeIdReplace(<-c, c)
+		go includeReplace(<-c, c)
+		go rewrite(<-c, c)
 
 	}
 
@@ -67,10 +70,88 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func rewrite(reqURI string, c chan Res) {
+func rewrite(txt Res, c chan Res) {
 	path := config.Path
-	alternate := config.Alternate
+	str := txt.Response
+	resErr := txt.Error
 	res := [][][][]byte{}
+	buf := []byte(str)
+
+	if len(path) > 0 && resErr == nil {
+		for _, v := range path {
+			regPath := regexp.MustCompile(v.K)
+			res = append(res, regPath.FindAllSubmatch(buf, 1))
+		}
+
+		if len(res) > 0 {
+			for _, v := range path {
+				str = strings.Replace(str, v.K, v.V, -1)
+			}
+		}
+	}
+
+	c <- Res{Error: resErr, Response: str}
+}
+
+func includeReplace(txt Res, c chan Res) {
+	regInc := regexp.MustCompile(`<!--#include ([a-z]+)="(\S+)" -->`)
+	str := txt.Response
+	resErr := txt.Error
+	buf := []byte(str)
+	res := regInc.FindAllSubmatch(buf, -1)
+
+	if len(res) > 0 && resErr == nil {
+		for _, v := range res {
+			incPath := string(v[2])
+			buf, err := os.ReadFile(incPath)
+			resErr = err
+
+			if err == nil {
+				incTxt := string(buf)
+				regString := fmt.Sprintf(`<!--#include ([a-z]+)="%s" -->`, incPath)
+				reg := regexp.MustCompile(regString)
+				str = reg.ReplaceAllString(str, incTxt)
+			}
+		}
+	}
+
+	c <- Res{Error: resErr, Response: str}
+}
+
+func includeIdReplace(txt Res, c chan Res) {
+	includeId := config.IncludeId
+	str := txt.Response
+	resBuf := []byte(str)
+	resErr := txt.Error
+	res := [][][][]byte{}
+
+	if resErr == nil {
+		for _, v := range includeId {
+			regString := fmt.Sprintf(`<("[^"]*"|'[^']*'|[^'">])*id="%s"("[^"]*"|'[^']*'|[^'">])*></("[^"]*"|'[^']*'|[^'">])*>`, v.K)
+			regInc := regexp.MustCompile(regString)
+			res = append(res, regInc.FindAllSubmatch(resBuf, 1))
+		}
+
+		if len(includeId) > 0 && len(res) > 0 {
+			for _, v := range includeId {
+				buf, err := os.ReadFile(v.V)
+				resErr = err
+
+				if resErr == nil {
+					incTxt := string(buf)
+					incTag := fmt.Sprintf(`<("[^"]*"|'[^']*'|[^'">])*id="%s"("[^"]*"|'[^']*'|[^'">])*></("[^"]*"|'[^']*'|[^'">])*>`, v.K)
+					reg := regexp.MustCompile(incTag)
+					str = reg.ReplaceAllString(str, incTxt)
+				}
+			}
+		}
+	}
+
+	c <- Res{Error: resErr, Response: str}
+}
+
+func getData(reqURI string, c chan Res) {
+	alternate := config.Alternate
 
 	if reqURI == "/" {
 		reqURI = "." + reqURI + "index.html"
@@ -90,81 +171,7 @@ func rewrite(reqURI string, c chan Res) {
 	buf, err := os.ReadFile(reqURI)
 	txt := string(buf)
 
-	if err == nil {
-		for _, v := range path {
-			regPath := regexp.MustCompile(v.K)
-			res = append(res, regPath.FindAllSubmatch(buf, 1))
-		}
-
-		if len(path) > 0 && len(res) > 0 {
-			for _, v := range path {
-				txt = strings.Replace(txt, v.K, v.V, -1)
-			}
-		}
-	}
-
 	c <- Res{Error: err, Response: txt}
-}
-
-func includeReplace(txt Res, c chan Res) {
-	regInc := regexp.MustCompile(`<!--#include ([a-z]+)="(\S+)" -->`)
-	buf := []byte(txt.Response)
-	res := regInc.FindAllSubmatch(buf, -1)
-	var ResErr error
-
-	if len(res) > 0 {
-		for _, v := range res {
-			incPath := string(v[2])
-			buf, err := os.ReadFile(incPath)
-			ResErr = err
-
-			if ResErr == nil {
-				incTxt := string(buf)
-				regString := fmt.Sprintf(`<!--#include ([a-z]+)="%s" -->`, incPath)
-				reg := regexp.MustCompile(regString)
-				txt.Response = reg.ReplaceAllString(txt.Response, incTxt)
-			}
-		}
-	}
-
-	c <- Res{Error: ResErr, Response: txt.Response}
-}
-
-func includeIdReplace(txt Res, c chan Res) {
-	includeId := config.IncludeId
-	res := [][][][]byte{}
-	buf := []byte(txt.Response)
-	var ResErr error
-
-	for _, v := range includeId {
-		regString := fmt.Sprintf(`<("[^"]*"|'[^']*'|[^'">])*id="%s"("[^"]*"|'[^']*'|[^'">])*></("[^"]*"|'[^']*'|[^'">])*>`, v.K)
-		regInc := regexp.MustCompile(regString)
-		res = append(res, regInc.FindAllSubmatch(buf, 1))
-	}
-
-	if len(includeId) > 0 && len(res) > 0 {
-		for _, v := range includeId {
-			buf, err := os.ReadFile(v.V)
-			ResErr = err
-
-			if ResErr == nil {
-				incTxt := string(buf)
-				incTag := fmt.Sprintf(`<("[^"]*"|'[^']*'|[^'">])*id="%s"("[^"]*"|'[^']*'|[^'">])*></("[^"]*"|'[^']*'|[^'">])*>`, v.K)
-				reg := regexp.MustCompile(incTag)
-				txt.Response = reg.ReplaceAllString(txt.Response, incTxt)
-			}
-		}
-	}
-
-	c <- Res{Error: ResErr, Response: txt.Response}
-}
-
-func getImage(reqURI string, c chan Res) {
-	reqURI = "." + reqURI
-	buf, err := os.ReadFile(reqURI)
-	str := string(buf)
-
-	c <- Res{Error: err, Response: str}
 }
 
 func init() {
